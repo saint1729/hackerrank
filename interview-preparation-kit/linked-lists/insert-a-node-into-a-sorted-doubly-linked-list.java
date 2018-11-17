904aecb6cc2191b5ed205ee6688d28d290897627
Sai Nikhil <tsnlegend@gmail.com>
2018-11-18 00:38:13 +0530

    Uploading 2 problems from topic "Sorting" of "The HackerRank Interview Preparation Kit"
    1) Insert a node at a specific position in a linked list
    2) Inserting a Node Into a Sorted Doubly Linked List

---
+++ interview-preparation-kit/linked-lists/insert-a-node-into-a-sorted-doubly-linked-list.java
@@ -0,0 +1,198 @@
+/*
+
+
+https://www.hackerrank.com/challenges/insert-a-node-into-a-sorted-doubly-linked-list/problem
+
+
+
+Inserting a Node Into a Sorted Doubly Linked List
+Problem
+Submissions
+Leaderboard
+Discussions
+Editorial
+Given a reference to the head of a doubly-linked list and an integer, , create a new DoublyLinkedListNode object having data value  and insert it into a sorted linked list while maintaining the sort.
+
+Function Description
+
+Complete the sortedInsert function in the editor below. It must return a reference to the head of your modified DoublyLinkedList.
+
+sortedInsert has two parameters:
+
+head: A reference to the head of a doubly-linked list of DoublyLinkedListNode objects.
+data: An integer denoting the value of the  field for the DoublyLinkedListNode you must insert into the list.
+Note: Recall that an empty list (i.e., where ) and a list with one element are sorted lists.
+
+Input Format
+
+The first line contains an integer , the number of test cases.
+
+Each of the test case is in the following format:
+
+The first line contains an integer , the number of elements in the linked list.
+Each of the next  lines contains an integer, the data for each node of the linked list.
+The last line contains an integer  which needs to be inserted into the sorted doubly-linked list.
+Constraints
+
+Output Format
+
+Do not print anything to stdout. Your method must return a reference to the  of the same list that was passed to it as a parameter.
+
+The ouput is handled by the code in the editor and is as follows: 
+For each test case, print the elements of the sorted doubly-linked list separated by spaces on a new line.
+
+Sample Input
+
+1
+4
+1
+3
+4
+10
+5
+Sample Output
+
+1 3 4 5 10
+Explanation
+
+The initial doubly linked list is:  .
+
+The doubly linked list after insertion is: 
+
+
+
+*/
+
+
+
+
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class DoublyLinkedListNode {
+        public int data;
+        public DoublyLinkedListNode next;
+        public DoublyLinkedListNode prev;
+
+        public DoublyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    static class DoublyLinkedList {
+        public DoublyLinkedListNode head;
+        public DoublyLinkedListNode tail;
+
+        public DoublyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            DoublyLinkedListNode node = new DoublyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+                node.prev = this.tail;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printDoublyLinkedList(DoublyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+    // Complete the sortedInsert function below.
+
+    /*
+     * For your reference:
+     *
+     * DoublyLinkedListNode {
+     *     int data;
+     *     DoublyLinkedListNode next;
+     *     DoublyLinkedListNode prev;
+     * }
+     *
+     */
+    static DoublyLinkedListNode sortedInsert(DoublyLinkedListNode head, int data) {
+
+        DoublyLinkedListNode newNode = new DoublyLinkedListNode(data);
+        
+        if(head.data >= data) {
+            newNode.next = head;
+            head.prev = newNode;
+            return newNode;
+        }
+        
+        DoublyLinkedListNode node = head;
+        while(node.next != null) {
+            if(node.next.data >= data) {
+                break;
+            }
+            node = node.next;
+        }
+        
+        DoublyLinkedListNode nextNode = node.next;
+        node.next = newNode;
+        newNode.next = nextNode;
+        
+        return head;
+
+    }
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            DoublyLinkedList llist = new DoublyLinkedList();
+
+            int llistCount = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < llistCount; i++) {
+                int llistItem = scanner.nextInt();
+                scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+                llist.insertNode(llistItem);
+            }
+
+            int data = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            DoublyLinkedListNode llist1 = sortedInsert(llist.head, data);
+
+            printDoublyLinkedList(llist1, " ", bufferedWriter);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
