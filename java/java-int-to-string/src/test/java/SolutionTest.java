4388a873d9ebd261cb98d5ac5768c4c3d8a8826c
Sai Nikhil <tsnlegend@gmail.com>
2020-03-31 08:14:31 +0530

    created a minimal gradle project for problems in "java" domain and removed existing scrap
    Signed-off-by: Sai Nikhil Thirandas <tsnlegend@gmail.com>

---
+++ java/java-int-to-string/src/test/java/SolutionTest.java
@@ -0,0 +1,67 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    private final InputStream systemIn = System.in;
+    private final PrintStream systemOut = System.out;
+
+    private FileInputStream testIn;
+    private ByteArrayOutputStream testOut;
+
+    @Before
+    public void setUpOutput() {
+        testOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testOut));
+    }
+
+    private void provideInput(String fileName) throws IOException {
+        testIn = new FileInputStream(fileName);
+        System.setIn(testIn);
+    }
+
+    private String getOutput() {
+        return testOut.toString();
+    }
+
+    private String outputAsString(String outputFile) throws IOException {
+        FileInputStream fstream = new FileInputStream(outputFile);
+        BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+        StringBuffer sb = new StringBuffer();
+        String line;
+
+        while ((line = br.readLine()) != null) {
+            sb.append(line);
+            sb.append("\n");
+        }
+
+        fstream.close();
+
+        return sb.toString();
+    }
+
+    @After
+    public void restoreSystemInputOutput() {
+        System.setIn(systemIn);
+        System.setOut(systemOut);
+    }
+
+    @Test
+    public void test() throws IOException {
+
+        String TEST_CASE_NUMBER = "00";
+
+        final String testString = "input" + TEST_CASE_NUMBER + ".txt";
+        provideInput(testString);
+        Solution.main(new String[0]);
+        String outputFromFile = outputAsString("output" + TEST_CASE_NUMBER + ".txt");
+        String outputFromMain = getOutput();
+
+        assertEquals(outputFromFile, outputFromMain); // .substring(0, outputFromMain.length() - 1));
+
+    }
+}
