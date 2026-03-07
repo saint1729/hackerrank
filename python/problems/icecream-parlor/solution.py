559185e6e9c066b1ada1d331c550eed866bfb667
Sai Nikhil Thirandas <saint.math.1729@gmail.com>
2026-03-07 12:00:00 -0800

    Add icecream-parlor solution

---
+++ python/problems/icecream-parlor/solution.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'icecreamParlor' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER m
+#  2. INTEGER_ARRAY arr
+#
+
+def icecreamParlor(m, arr):
+    seen = {}
+    for i, cost in enumerate(arr):
+        complement = m - cost
+        if complement in seen:
+            return [seen[complement] + 1, i + 1]
+        seen[cost] = i
+    return []
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        m = int(input().strip())
+
+        n = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = icecreamParlor(m, arr)
+
+        fptr.write(' '.join(map(str, result)))
+        fptr.write('\n')
+
+    fptr.close()
